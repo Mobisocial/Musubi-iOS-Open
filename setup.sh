15e9ea1d5377e2355884cb7aa0faeeba52e682ee
T.J. Purtell <tj@vimoin.com>
2012-05-12 11:52:21 -0700

    add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#this script initializes a clean checkout of musubi and builds the libraries
+#that are bundled in source code form.  after you checkout for the first time
+
+
+echo FETCHING DEPENDENCIES
+git submodule update --init
+
+echo GENERATING GMP SOURCE
+pushd Libraries/GMP
+xcodebuild -project gmpbuild.xcodeproj/ -target gen-src
+popd
+
+echo BUILDING IBE LIBRARY
+xcodebuild -scheme IBE -scheme IBE-release 
+
+echo BUILDING 320 LIBRARY
+xcodebuild -scheme Three20 -scheme Three20-Release
