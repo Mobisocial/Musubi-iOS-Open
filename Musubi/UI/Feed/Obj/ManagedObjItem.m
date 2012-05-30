25a87c117a946737b664c0c4fe97e6429b6386bc
Ben Dodson <bjdodson@Bens-MacBook-Pro.local>
2012-05-29 22:24:14 -0700

    managed obj item

---
+++ Musubi/UI/Feed/Obj/ManagedObjItem.m
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012 The Stanford MobiSocial Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+//
+//  IntroductionObjItem.m
+//  musubi
+//
+//  Created by Ben Dodson on 5/29/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import "ManagedObjItem.h"
+#import "MObj.h"
+
+@implementation ManagedObjItem
+
+- (id)initWithManagedObj:(MObj*)mObj
+{
+    self = [super init];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
++ (CGFloat)renderHeightForItem:(FeedItem *)item {
+    return 80;
+}
+@end
